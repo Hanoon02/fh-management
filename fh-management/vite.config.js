c79dfd846d29e78c636c634c8351c3edac2dbe66
Hanoon02 <ahmedhanoon02@gmail.com>
2025-03-19 08:51:00 +0530

    update

--- fh-management/vite.config.js
+++ fh-management/vite.config.js
@@ -7,10 +7,4 @@ export default {
       "@": "/src",
     },
   },
-  server: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
-      "Cross-Origin-Embedder-Policy": "require-corp"
-    }
-  }
 };
